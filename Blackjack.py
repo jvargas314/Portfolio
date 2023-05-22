f30fd33e6a9fb5d5211dfc2ba3f933bb5f369ceb
jvargas314 <130497294+jvargas314@users.noreply.github.com>
2023-05-22 14:13:37 -0600

    Update and rename Copy Apr 11 Modified.py to Blackjack.py

--- Blackjack.py
+++ Blackjack.py
@@ -1,3 +1,6 @@
+#In an Intro to Python course I took, we were challenged to write a code that would allow a user to play a game of blackjack along with three computer players.
+#Below is the code I came up with. I first defined all my functions, then called them individually to commence the game.
+
 def deal(players): 
         cardsout=[] 
         i=1 
@@ -16,63 +19,37 @@ def deal(players):
         print(sorted(cardsout)) #check
 
 def find_value(cards): 
-
-    """It finds the total value of the cards""" 
-
     card_val = 0
-
     ace = False 
-
     value_dict = {'A': 11, 'K': 10, 'Q': 10, 'J': 10, '10': 10, 
-
     '9': 9, '8': 8, '7': 7, '6': 6, '5': 5, '4': 4, '3': 3, '2': 2, } 
-
- 
-
     for i in range(0, len(cards), 2):
         temp = value_dict[str(cards[i])]
         card_val += temp
-
         if cards[i] == 'A': 
-
             ace = True
-
-
         if card_val > 21 and ace: 
-    
             card_val -= 10
-
     return card_val 
 
 
 def bet(pchips):
-    
     for key in pchips:
-        
         if key != 'p2':
             (pchips[key])-=5
         else:
             try: 
-
                 p2temp = int(input('How many chips would you like to bet?  ')) 
-
             except ValueError: 
-
                 print("Please enter an interger") 
-
             if p2temp < 5: 
-
                     print('Sorry, you must bet a minimum of $5 ')
                     bet(pchips)
-
             elif p2temp > pchips[key]: 
-
                     print('Sorry, your bet cannot exceed', pchips[key])
                     bet(pchips)
             else:
                 pchips['p2']-=p2temp
-            
-            
     print(pchips)
 
         
@@ -90,7 +67,6 @@ order=list(range(1,53))
 random.shuffle(order) 
 
 players={'cpu1':[],'p2':[],'cpu3':[],'cpu4':[],'dealer':[]} 
-
 pchips={'p2':100,'cpu1':100,'cpu3':100,'cpu4':100}
 
 
@@ -107,12 +83,5 @@ for player in pchips:
 if (max_player == "cpu1" or max_player == "cpu3" or max_player == "cpu4"):
     pchips[max_player] += 10
 else:
-    pchips[max_player] += 7#TODO: add the user bet
+    pchips[max_player] += 7 #Incomplete: add the user bet
         
-    
-    
-    
-    
-    
-     
- 
